10158fed478e01fd54300187993cb4ba32eb392d
Helen Khoda <helen_khoda@yahoo.com>
2019-08-16 10:01:37 -0500

    lovely routes

---
+++ README.md
@@ -0,0 +1,17 @@
+# Create React Express App
+
+## About This Boilerplate
+
+This app allows for a Node/Express/React app which can be easily deployed to Heroku.
+
+The front-end React app will auto-reload as it's updated via webpack dev server, and the backend Express app will auto-reload independently with nodemon.
+
+## Starting the app locally
+
+Start by installing front and backend dependencies. While in this directory, run the following command:
+npm install
+npm start
+my app should now be running on <http://localhost:3001>. The Express server should intercept any AJAX requests from the client.
+ 
+ Here is how it appear on localhost:
+ ![Homepage](https://github.com/helenkhoda2019/EATDABURGER/blob/master/final.png)
---
+++ client/src/App.js
@@ -0,0 +1,107 @@
+import React from "react";
+import Image from "./components/Image";
+import API from "./utils/API";
+import './App.css';
+
+// var my_images = [
+//   { id: 0, alt:"bob", src: "https://vignette.wikia.nocookie.net/spongebobgalaxy/images/0/07/SpongeBob_SquarePants.png/revision/latest?cb=20171228024014" },
+//   { id: 1, alt: "krabs", src: "https://vignette3.wikia.nocookie.net/vsbattles/images/8/80/Mr._Krabs.png/revision/latest?cb=20150919162131" },
+//   { id: 2, alt: "squid", src: "https://vignette2.wikia.nocookie.net/fictionalcharacters/images/a/ac/Squidward.png/revision/latest?cb=20131121012626" },
+//   { id: 3, alt: "onim", src: "https://s-media-cache-ak0.pinimg.com/originals/fe/32/49/fe32495d45283cd6860ae122f0aeaad9.png" }
+// ]
+
+
+class App extends React.Component {
+    state = {
+        score: 0,
+        images: [],
+        images_clicked: [],
+        searchterm: "kites"
+    }
+    componentDidMount() {
+        API.search(this.state.searchterm)
+            .then(res => {
+                console.log(this.state.images);
+                this.setState({
+                    images: res.data.data
+                })
+            })
+    }
+
+    clickme = (data) => {
+        let id = data.id;
+        let mongo_data = {
+            src: data.src,
+            id: data.id,
+            alt: data.alt
+        };
+
+        if (this.state.images_clicked.indexOf(id) === -1) {
+            //image doesn't exist
+            this.setState({
+                score: this.state.score + 1,
+                images_clicked: [...this.state.images_clicked, id],
+                images: this.state.images.sort(() => Math.random() - 0.5)
+            });
+            //mongo expects id, src, alt
+            API.upload_data(mongo_data)
+                .then(res => {
+                    console.log(res)
+                });
+        } else {
+            this.setState({
+                score: 0,
+                images_clicked: [],
+                images: this.state.images.sort(() => Math.random() - 0.5)
+            })
+        }
+
+    }
+
+    searchGiphy = (event) => {
+        let value = event.target.value.trim();
+        this.setState({
+            searchterm: value
+        });
+        API.search(this.state.searchterm)
+            .then(res => {
+                this.setState({
+                    images: res.data.data
+                })
+            });
+    }
+    render() {
+        return (
+            
+                <div class="nav">
+                <header className="header text-center">
+                        <h1>Clicky Game!</h1>
+                      
+                        <h2>Click on an image to earn points!!</h2>
+                    </header>
+
+                    {/* <nav className="navbar navbar-light bg-light"> */}
+                        <span className="navbar-brand mb-0 h1">React Clicky Game</span> 
+                        <span className="navbar-text">Score: {this.state.score}</span>
+                        <form className="form-inline my-2 my-lg-0">
+                            <div>{this.state.searchterm}</div>
+                            <input className="form-control mr-sm-2" type="text" value={this.state.searchterm} onChange={this.searchGiphy} placeholder="Search Giphys" />
+                        </form>
+                    {/* </nav> */}
+                    <br />
+                    {/* <header className="header text-center">
+                        <h1>Clicky Game!</h1>
+                        <h2>Search Giphys first.</h2>
+                        <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+                    </header> */}
+
+                    {this.state.images.map((img) => (
+                        <Image id={img.id} alt={img.slug} src={img.images.fixed_height_still.url} key={img.id} clickme={this.clickme} />)
+                    )}
+                </div>
+        
+        )
+    }
+}
+
+export default App;
